1dcfb1f2fdd57fcfe105ec0d8b862d35db00e9b1
Allan Pablo <allanpablo@gmail.com>
2021-04-22 01:35:12 -0300

    SPA/ SSR/ SSG

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Header } from '../components/Header';
+import '../styles/global.scss';
+import styles from '../styles/app.module.scss';
+import { Player } from '../components/Player';
+function MyApp({ Component, pageProps }) {
+  return(
+    <div className= {styles.wrapper}>
+      <main>
+      <Header />
+      <Component />
+      </main>
+      <Player />
+    </div>
+  )
+}
+
+export default MyApp
